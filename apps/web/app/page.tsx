c18eb1c5e6fb2f965a45c10f59a087247cf351fd
Petros Kyriakou <9pitops@gmail.com>
2023-08-05 10:41:45 +0300

    add button component and rendering example

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,24 @@
-import { Button, Header } from "ui";
+import { Header } from "ui";
+import { Button } from "@emailcraft/components";
 
 export default function Page() {
   return (
-    <>
+    <div style={{ background: "black" }}>
       <Header text="Web" />
-      <Button />
-    </>
+      <Button href="https://example.com" style={button} pY={19}>
+        Click me
+      </Button>
+    </div>
   );
 }
+
+const button = {
+  backgroundColor: "#ff5a5f",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "18px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  width: "100%",
+};
